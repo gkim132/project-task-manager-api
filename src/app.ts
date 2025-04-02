496cfe2ce0db4ef3ecdb524e430fadc52582dc39
gkim <gukkim132@gmail.com>
2025-04-02 12:59:38 -0500

    setup local Serverless testing

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,8 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.use("/tasks", taskRouter);
-app.use("/user", userRouter);
+app.use(`/tasks`, taskRouter);
+app.use(`/user`, userRouter);
 
 app.all("*", (req, res, next) => {
   next(new BaseError(`Can't find ${req.originalUrl} on this server!`, 404));
---
+++ src/lambda.ts
@@ -0,0 +1,21 @@
+import serverless from "serverless-http";
+import mongoose from "mongoose";
+import app from "./app";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+let isConnected = false;
+
+const connectToDatabase = async () => {
+  if (!isConnected) {
+    await mongoose.connect(MONGO_URI!);
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  }
+};
+
+export const handler = async (event: any, context: any) => {
+  await connectToDatabase();
+  const handler = serverless(app);
+  return handler(event, context);
+};
